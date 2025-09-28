8296d6f408df40b1c7daf550dc066c21171eaa1d
Elleni-Bisrat <ellenibisrat16@gmail.com>
2025-09-28 22:59:52 +0300

    updated

--- alx-project-0x03/interface/index.tsx
+++ alx-project-0x03/interface/index.tsx
@@ -1,8 +1,8 @@
 export interface ButtonProps {
-  buttonLabel: string;
-  buttonSize?: string;
-  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green';
-  action?: () => void;
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
 }
 export interface PageRouteProps {
   pageRoute: string;
